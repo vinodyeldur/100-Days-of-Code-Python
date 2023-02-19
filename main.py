0e61a959cf898a9c2703117c9073659c794e8879
Vinod1999 <109039225+vinodyeldur@users.noreply.github.com>
2023-02-19 04:25:16 +0000

    Debugging practice

--- main.py
+++ main.py
@@ -29,21 +29,23 @@ import random
 game_images = [rock, paper, scissors]
 
 user_choice =  int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for scissor"))
-print(game_images[user_choice])
-
-computer_choice = random.randint(0, 2)
-print("Computer_choice:")
-print(game_images[computer_choice])
-
 if user_choice >= 3 or user_choice < 0:
   print("You typed an invalid number, you lose!")
-elif user_choice == 0 and computer_choice == 2:
-  print("You win!")
-elif computer_choice == 0 and user_choice == 2:
-  print("You lose")
-elif user_choice >= 3 or user_choice < 0:
-  print("You typed an invalid number, you lose!")
-elif computer_choice > user_choice:
-  print("You lose")
-elif user_choice > computer_choice:
-  print("You Win!")
+else: 
+  print(game_images[user_choice])
+  
+  computer_choice = random.randint(0, 2)
+  print("Computer_chose:")
+  print(game_images[computer_choice])
+  
+  
+  if user_choice == 0 and computer_choice == 2:
+    print("You win!")
+  elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+  elif computer_choice > user_choice:
+    print("You lose")
+  elif user_choice > computer_choice:
+    print("You Win!")
+  elif computer_choice == user_choice:
+    print("It's a draw")
